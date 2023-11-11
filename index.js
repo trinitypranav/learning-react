ddc281d7455f0638779776b6633c91e896c7d256
Pranav Jadhav <trinitypranav@gmail.com>
2023-11-12 00:47:44 +0530

    Using parcel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./src/App"
+import App from "./src/App";
 
+console.log("starting my app");
 ReactDom.render(<App />, document.getElementById("root"));
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () =>{
     return (
         <h1>
-            Welcome to React App thats build using Webpack and Babel separately
+            Welcome to React App thats build using Parcel!
         </h1>
     )
 }
