3db52489fa3debf0026c5eb28556078ce08a89d5
Pranav Jadhav <trinitypranav@gmail.com>
2023-12-03 21:41:46 +0530

    created custom hook to fetch restaurant details

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -1,51 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { cloudinaryImageURL } from "../utils/config";
+import useRestaurantDetails from "../utils/customHooks/useRestaurantDetails";
+import Shimmer from "./Shimmer";
 
 const RestaurantDetails = () => {
   let { id } = useParams();
-  //console.log(id);
-  let [restaurantDetails, setRestaurantDetails] = useState({});
-  let [restaurantMenu, setRestaurantMenu] = useState([]);
 
-  useEffect(() => {
-    getRestaurantDetails(id);
-  }, []);
-
-  const getRestaurantDetails = async (id) => {
-    let data = await fetch(
-      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4761852&lng=73.87455469999999&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`
+  let restaurantDetails = {};
+  let restaurantMenu = [];
+  let json = useRestaurantDetails(id);
+  restaurantDetails = json?.data?.cards[0]?.card?.card?.info;
+  restaurantMenu =
+    json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards?.map(
+      (item) => {
+        return item.card.info.name;
+      }
     );
-    let json = await data.json();
-    console.log(json?.data?.cards[0]?.card?.card?.info);
-    setRestaurantDetails(json?.data?.cards[0]?.card?.card?.info);
-    setRestaurantMenu(
-      json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards?.map(
-        (item) => item.card.info.name
-      )
-    );
-  };
 
   return (
     <div className="restaurantDetailsContainer">
       <div className="restaurantInfo">
         <h1>
-          {restaurantDetails.name}{" "}
+          {restaurantDetails?.name}{" "}
           <span
             style={{ color: "#4CAF50", fontSize: "large", marginLeft: "5px" }}
           >
             {" "}
-            &#9733;{restaurantDetails.avgRating} (
-            {restaurantDetails.totalRatingsString})
+            &#9733;{restaurantDetails?.avgRating} (
+            {restaurantDetails?.totalRatingsString})
           </span>
         </h1>
         <img
           className="restaurantImage"
-          src={cloudinaryImageURL + restaurantDetails.cloudinaryImageId}
+          src={cloudinaryImageURL + restaurantDetails?.cloudinaryImageId}
         />
         <h2>
-          Area: {restaurantDetails.areaName}, Locality :{" "}
-          {restaurantDetails.locality}
+          Area: {restaurantDetails?.areaName}, Locality :{" "}
+          {restaurantDetails?.locality}
         </h2>
         <span>Cuisines: {restaurantDetails?.cuisines?.join(", ")}</span> <br />
         <span> {restaurantDetails?.costForTwoMessage}</span> <br />
---
+++ src/utils/customHooks/useRestaurantDetails.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const useRestaurantDetails = (id) => {
+  let [restaurantDetails, setRestaurantDetails] = useState({});
+
+  useEffect(() => {
+    getRestaurantDetails(id);
+  }, []);
+
+  const getRestaurantDetails = async (id) => {
+    let data = await fetch(
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4761852&lng=73.87455469999999&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`
+    );
+    let json = await data.json();
+    // console.log("sending data " + JSON.stringify(json));
+    setRestaurantDetails(json);
+  };
+
+  return restaurantDetails;
+};
+
+export default useRestaurantDetails;
