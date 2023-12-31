43b34658aa4d2e318674fa902a1eb30feacf2407
Pranav Jadhav <trinitypranav@gmail.com>
2023-12-31 19:56:33 +0530

    config file updated with redux store

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,13 @@
 //any hardcoded values should be added here
 //https://corsproxy.io/?
 
-export const getAllRestaurantsURL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.481361&lng=73.8754846&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const getAllRestaurantsURL = `https://api.allorigins.win/get?url=${encodeURIComponent(
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.481361&lng=73.8754846&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+)}`;
 
 export const cloudinaryImageURL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+// `https://api.allorigins.win/get?url=${encodeURIComponent(
+//         "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+//       )}`
